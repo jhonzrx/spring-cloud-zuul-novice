b8c7f8b937e05a07bf620382e8fbc11d1b4f09f6
bowen.yan <yanbowen-pbj@sinosig.com>
2018-08-22 10:06:21 +0800

    idea打开时报错

--- uaa-service/src/main/java/com/rsoft/uaa/dto/User.java
+++ uaa-service/src/main/java/com/rsoft/uaa/dto/User.java
@@ -1,20 +1,21 @@
 package com.rsoft.uaa.dto;
 
-import com.rsoft.uaa.web.LoginController;
-
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class User{
-	private String userid;
-	private String username;
-	private String password;
-	public User(String userid, String username, String password) {
-		super();
-		this.userid = userid;
-		this.username = username;
-		this.password = password;
-	}
+@AllArgsConstructor
+public class User {
+    private String userid;
+    private String username;
+    private String password;
+
+//    public User(String userid, String username, String password) {
+//        super();
+//        this.userid = userid;
+//        this.username = username;
+//        this.password = password;
+//    }
 }
